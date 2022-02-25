d7095cba2c6ec75fa6f473695d7ca37c504164c1
Kagan Inan <contact@kaaninan.net>
2022-02-25 18:20:33 +0300

    added build script for mac

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ echo "Working.."
 rm -rf extra_resources
 mkdir -p extra_resources/.platformio
 mkdir -p extra_resources/yetenek12-library/
-rsync -a yetenek-ide-pio-mac/* extra_resources/.platformio/*
-rsync -a yetenek12-library/* extra_resources/yetenek12-library/*
+rsync -a yetenek-ide-pio-mac/* extra_resources/.platformio/
+rsync -a yetenek12-library/* extra_resources/yetenek12-library/
 echo "Done"
