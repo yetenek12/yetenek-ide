f46d7a44e5cee45c0d42b999b52b028585c5b052
emreon <onrat.emre@gmail.com>
2022-03-28 10:06:23 +0300

    single instance mode

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 const { app, BrowserWindow } = require('electron');
 const { stubFalse } = require('lodash');
 const url = require('url');
-const server = require('./server')
+
+// https://stackoverflow.com/questions/35916158/how-to-prevent-multiple-instances-in-electron
+const gotTheLock = app.requestSingleInstanceLock()
+
+if (!gotTheLock) {
+    console.log('There is another instance running. Quitting...')
+    app.quit()
+} else {
+    app.on('second-instance', (event, commandLine, workingDirectory) => {
+        console.log('Another instance started. Focusing on this...')
+
+        // Someone tried to run a second instance, we should focus our window.
+        if (global.win) {
+            if (global.win.isMinimized()) {
+                global.win.restore()
+            }
+            global.win.focus()
+        }
+    })
+}
 
 function createWindow() {
+    const server = require('./server')
 
     // https://serialport.io/docs/guide-installation/
     global.win = new BrowserWindow({
@@ -25,16 +45,18 @@ function createWindow() {
     // win.webContents().openDevTools();
 }
 
-app.whenReady().then(createWindow);
+if(gotTheLock){
+    app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
-
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
-});
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') {
+            app.quit();
+        }
+    });
+    
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+}
